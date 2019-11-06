a04cba274c6ad7be4046b80e77ddc2d3d82969f4
shirleywangyy <47951319+shirleywangyy@users.noreply.github.com>
2019-11-06 15:45:48 -0500

    Add files via upload

--- project1/js/scripts.js
+++ project1/js/scripts.js
@@ -1,5 +1,142 @@
 $(document).keydown(function(keyPressed) {
- if (keyPressed.keyCode == 65) {
-   $("body").append("<img class='svg' src='images/Corgi_A.svg'>");
+    
+    
+     if (keyPressed.keyCode === 8) {
+   $(".newDiv img:last-child").remove();
  }
+
+
+     if (keyPressed.keyCode == 46) {
+   $(".newDiv img:last-child").remove();
+ }
+    
+ if (keyPressed.keyCode == 65) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_A.svg'>");
+ }
+    
+    
+   if (keyPressed.keyCode == 66) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_B.svg'>");
+ }
+  
+     if (keyPressed.keyCode == 67) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_C.svg'>");
+ }
+      
+      if (keyPressed.keyCode == 68) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_D.svg'>");
+ }
+        
+     if (keyPressed.keyCode == 69) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_E.svg'>");
+ }
+      
+    
+     if (keyPressed.keyCode == 70) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_F2.svg'>");
+ }
+      
+    
+     if (keyPressed.keyCode == 71) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_G.svg'>");
+ }
+      
+       if (keyPressed.keyCode == 72) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_H.svg'>");
+ }
+    
+       if (keyPressed.keyCode == 73) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_I.svg'>");
+ }
+    
+       if (keyPressed.keyCode == 74) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_J.svg'>");
+ }
+    
+           if (keyPressed.keyCode == 75) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_K.svg'>");
+ }
+    
+    
+    if (keyPressed.keyCode == 76) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_L.svg'>");
+ }
+    
+    if (keyPressed.keyCode == 77) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_M.svg'>");
+ }
+    
+    
+    if (keyPressed.keyCode == 78) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_N.svg'>");
+ }
+    
+    
+   if (keyPressed.keyCode == 79) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_O.svg'>");
+ }
+    
+
+    if (keyPressed.keyCode == 80) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_P.svg'>");
+ }
+    
+    
+        if (keyPressed.keyCode == 81) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_Q.svg'>");
+ }
+    
+    
+    if (keyPressed.keyCode == 82) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_R.svg'>");
+ }
+    
+        
+    if (keyPressed.keyCode == 83) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_S.svg'>");
+ }
+    
+        
+    if (keyPressed.keyCode == 84) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_T.svg'>");
+ }
+    
+            
+    if (keyPressed.keyCode == 85) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_U.svg'>");
+ }
+    
+            
+    if (keyPressed.keyCode == 86) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_V.svg'>");
+ }
+    
+        if (keyPressed.keyCode == 87) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_W.svg'>");
+ }
+    
+        if (keyPressed.keyCode == 88) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_X.svg'>");
+ }
+    
+        if (keyPressed.keyCode == 89) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_Y.svg'>");
+ }
+    
+    
+        if (keyPressed.keyCode == 90) {
+   $("#newDiv").append("<img class='svg' src='images/Corgi_Z.svg'>");
+ }
+    
+    
+        if (keyPressed.keyCode == 32) {
+   $("#newDiv").append("<img class='svg' src='images/Space.svg'>");
+ }
+    
+    
+    
+    
 });
+
+
+
